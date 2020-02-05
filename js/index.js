2397393be8b6645a7f407ccdb5dba210ae82116a
Justina <justina@Justinas-MacBook-Pro.local>
2020-02-05 23:30:33 +0200

    Removed lightbox on mobile

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ window.onload = function() {
 
         // *******************    STICKY SOCIAL-BAR    *******************
         var socialBar = document.getElementById("social-bar");
+        var media769px = window.matchMedia("(min-width: 769px)");
+        addRemoveSocialBar(media769px);
+        media769px.addListener(addRemoveSocialBar);
 
         function addRemoveSocialBar(media769px) {
             if (media769px.matches) {
@@ -15,11 +18,6 @@ window.onload = function() {
             }
         }
 
-        var media769px = window.matchMedia("(min-width: 769px)");
-        addRemoveSocialBar(media769px);
-        media769px.addListener(addRemoveSocialBar);
-        
-
         // *******************    PARALLAX HERO IMG    *******************
         var parallaxElementHero = document.querySelector('.parallax-hero');
         // var parallaxElementBg = document.querySelector('.parallax-bg');
@@ -30,7 +28,27 @@ window.onload = function() {
 
         parallaxElementHero.style.transform = 'translate3d(0px, ' + scrollRateHero + 'px, 0px)';
         // parallaxElementBg.style.transform = 'translate3d(0px, ' + scrollRateBg + 'px, 0px)';
+    }
 
+    // *******************    DISABLE LIGHTBOX ON MOBILE    *******************
+    var media576px = window.matchMedia("(max-width: 576px)");
+    removeLightbox(media576px);
+    media576px.addListener(removeLightbox);
+    
+    function removeLightbox(media576px) {
+        if (media576px.matches) {
+            // function removeAttributes(element, ...attrs) {
+            //     attrs.forEach(attr => element.removeAttribute(attr))
+            // }
+            // removeAttributes(document.querySelector('.section__gallery--item'), 'href', 'data-lightbox');
+            var galleryItem = document.getElementsByClassName('section__gallery--item');
+
+            for (i = 0; i < galleryItem.length; i++) {
+                galleryItem[i].removeAttribute('href');
+                galleryItem[i].removeAttribute('data-lightbox');
+
+            }
+        } 
     }
 
 }
